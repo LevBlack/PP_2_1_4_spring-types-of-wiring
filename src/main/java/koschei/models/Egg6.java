6b16d453bb834c43ef308a280bfd5766937c8aa8
LevBlack <flah7796@gmail.com>
2022-09-30 13:24:07 +0500

    main 1.1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class Egg6 {
 
-    private final Needle7 needle;
-
+    private Needle7 needle;
     @Autowired
-    public Egg6(Needle7 needle) {
-        this.needle = needle;
-    }
+    public void setNeedle(Needle7 needle) {this.needle = needle; }
 
     @Override
     public String toString() {
